cc92e713278cc6d88f854eea33d10ffdb1514111
William Marlette <williammarlette3@gmail.com>
2025-04-16 22:29:48 -0400

    add clothes card section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import { useState } from "react";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" });
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,25 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
 import "./Main.css";
+import { defaultClothingItems } from "../../utils/constants.js";
+import ItemCard from "../ItemCard/ItemCard.jsx";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
       <section className="cards">
-        Today is 75 &deg; F / You may want to wear:
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+        <ul className="cards__list">
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </main>
   );
