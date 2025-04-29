67f4ca7c29365722846dd6f106668059cf341dad
William Marlette <williammarlette3@gmail.com>
2025-04-29 01:22:15 -0400

    add mobile design style

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,7 +6,17 @@ function SideBar() {
   return (
     <div className="sidebar">
       <img src={avatar} alt="User's avatar" className="sidebar__avatar" />
-      <p className="sidebar__username">Terrance Tegene</p>
+      <div className="sidebar__user-container">
+        <p className="sidebar__username">Terrance Tegene</p>
+        <div className="sidebar__mobile-options">
+          <button type="button" className="sidebar__mobile-option">
+            Change profile data
+          </button>
+          <button type="button" className="sidebar__mobile-option">
+            Log out
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
