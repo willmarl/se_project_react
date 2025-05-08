011637736156220181693feb36db3f9bd6e17abf
William Marlette <williammarlette3@gmail.com>
2025-05-08 15:37:35 -0400

    update footer to update year dynamically

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">Developed by William Marlette</p>
-      <p className="footer__text">2025</p>
+      <p className="footer__text">{new Date().getFullYear()}</p>
     </footer>
   );
 }
